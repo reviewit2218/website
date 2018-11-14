2d99823252a7f495d3a9ee74795ce677893aa044
Rakesh Rajan <rakeshrajan@Rakeshs-Mac-mini.local>
2018-11-14 22:07:14 +0530

    upload

---
+++ precache-manifest.c3cc4cdebcc96c525f722730d3f62da3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9e6b094977aac5bc12d",
+    "url": "/dataprorrisi/static/css/main.568aacac.chunk.css"
+  },
+  {
+    "revision": "c9e6b094977aac5bc12d",
+    "url": "/dataprorrisi/static/js/main.c9e6b094.chunk.js"
+  },
+  {
+    "revision": "733b26bb8205aa9eb854",
+    "url": "/dataprorrisi/static/js/1.733b26bb.chunk.js"
+  },
+  {
+    "revision": "01d56900c723e30604b6",
+    "url": "/dataprorrisi/static/js/runtime~main.01d56900.js"
+  },
+  {
+    "revision": "3ee62e9a05eda34580244b1259c075d4",
+    "url": "/dataprorrisi/static/media/finance.3ee62e9a.png"
+  },
+  {
+    "revision": "a10983eb092cb81a78507559d4ffbaff",
+    "url": "/dataprorrisi/static/media/report.a10983eb.svg"
+  },
+  {
+    "revision": "a40937394fd4701e0a748cc9430d1d47",
+    "url": "/dataprorrisi/static/media/security.a4093739.svg"
+  },
+  {
+    "revision": "6138cff2e618f582d2a20c7982fd9d82",
+    "url": "/dataprorrisi/static/media/john.6138cff2.png"
+  },
+  {
+    "revision": "fdcd58c2a4d696b4c3b40c4a1a7bb585",
+    "url": "/dataprorrisi/static/media/james.fdcd58c2.png"
+  },
+  {
+    "revision": "67095cdff41ef40b87ffd02f6ee32c51",
+    "url": "/dataprorrisi/static/media/lisa.67095cdf.png"
+  },
+  {
+    "revision": "95c71f8008b8d015ae389b7785d40451",
+    "url": "/dataprorrisi/static/media/success.95c71f80.png"
+  },
+  {
+    "revision": "6704400409d5d654bc7222b805774a9f",
+    "url": "/dataprorrisi/static/media/mailbox.67044004.svg"
+  },
+  {
+    "revision": "e1c1f153ce5447810714eab7d52db838",
+    "url": "/dataprorrisi/static/media/finance1.e1c1f153.png"
+  },
+  {
+    "revision": "19312e71f299cdd772d4eb77ade284d9",
+    "url": "/dataprorrisi/static/media/security1.19312e71.png"
+  },
+  {
+    "revision": "58d0a0d87417fd2e3fb345778f28df52",
+    "url": "/dataprorrisi/static/media/report1.58d0a0d8.png"
+  },
+  {
+    "revision": "07141757a36f99c2e1d0401aacbddad0",
+    "url": "/dataprorrisi/static/media/secright.07141757.png"
+  },
+  {
+    "revision": "69aff61d08c710928b2b7dcc1f1dab63",
+    "url": "/dataprorrisi/static/media/Bryant Regular.69aff61d.woff"
+  },
+  {
+    "revision": "7ff188f4125dc348396a6e96730f2d30",
+    "url": "/dataprorrisi/static/media/Bryant Regular.7ff188f4.eot"
+  },
+  {
+    "revision": "e846eb47d750261aa61b2c7b22ffb8e9",
+    "url": "/dataprorrisi/static/media/Bryant Regular.e846eb47.ttf"
+  },
+  {
+    "revision": "4f6a08658382de23a983a6ca3893378e",
+    "url": "/dataprorrisi/static/media/secleft.4f6a0865.png"
+  },
+  {
+    "revision": "5d828682e783b41b3dc1bd36f25a1972",
+    "url": "/dataprorrisi/static/media/Bryant Regular.5d828682.svg"
+  },
+  {
+    "revision": "a5b8dd657327b3a3c0924b00c1b6c683",
+    "url": "/dataprorrisi/static/media/topbg.a5b8dd65.png"
+  },
+  {
+    "revision": "344da9a40e08f89f6185261a124f6dfb",
+    "url": "/dataprorrisi/static/media/btmbg.344da9a4.png"
+  },
+  {
+    "revision": "97141d2756da1bb2cd408916de6d20bf",
+    "url": "/dataprorrisi/index.html"
+  }
+];
